7e927608b99c979cfd814d9f32ea9cb4734d2bbe
lerosua <lerosua@gmail.com>
2016-01-29 10:25:21 +0800

    Initial Commit

---
+++ OJAAssetDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OJAAssetDemo
+//
+//  Created by lerosua on 16/1/29.
+//  Copyright © 2016年 lerosua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
